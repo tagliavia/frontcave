13c15058fdb39947d62eeb4b24cadbcd88074e2d
Riccardo Tagliavia <rtagliavia@gmail.com>
2021-04-20 15:24:01 -0500

    Bootstrap configurado para el proyecto Corrimos: npm install react-bootstrap npm install bootstrap npm install sass Se creo archivo _document.js Se creo archivo styles.scss, se importo bootstrap y una fuente de google Se importo styles.scss en _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import '../styles/globals.css'
+/**
+ * este es el origen de nuestra app y aqui incluiremos los pquetes
+ * y logica que se aplicara a todo el site
+ */
+import '../styles/styles.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
